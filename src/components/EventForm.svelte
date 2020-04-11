8962b80a2d307eccf1bc3553cfc7299b00a432ed
Nick <nikuhluss@gmail.com>
2020-04-10 19:53:29 -0500

    add New Event button and form

---
+++ src/components/EventForm.svelte
@@ -0,0 +1,45 @@
+<script>
+    import Submit from './Submit.svelte'
+
+    let availableEventTypes = [
+        {id: "Rainout", value: "Rainout"},
+    ];
+
+    export let title = null;
+    export let description = null;
+    export let eventType = 'Rainout';
+
+</script>
+
+
+
+<form class="form">
+    <div class="field">
+        <label class="label">Title</label>
+        <div class="control">
+            <input class="input" type="text" placeholder="Title" bind:value={title} />
+        </div>
+    </div>
+
+    <div class="field">
+        <label class="label">Description</label>
+        <div class="control">
+            <input class="input" type="text-box" placeholder="Description" bind:value={description} />
+        </div>
+    </div>
+
+    <div class="field">
+        <label class="label">Event Type</label>
+        <div class="control">
+            <div class="select">
+                <select bind:value={eventType}>
+                    {#each availableEventTypes as eventType}
+                        <option value={eventType.id}>{eventType.value}</option>
+                    {/each}
+                </select>
+            </div>
+        </div>
+    </div>
+
+    <Submit payload={{title, description, eventType}} on:submit on:cancel />
+</form>
