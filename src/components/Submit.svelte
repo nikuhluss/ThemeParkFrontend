f224618e9b24e37fcf17ed282f227d38ec35f976
Gibbsface <cole.gibbs@gmail.com>
2020-04-16 01:40:35 -0500

    made the login page look good

--- src/components/Submit.svelte
+++ src/components/Submit.svelte
@@ -1,23 +1,34 @@
 <script>
-    import { createEventDispatcher } from 'svelte';
+  import { createEventDispatcher } from "svelte";
 
-    export let payload = {};
-    export let disabled = false;
-    export let bulmaSubmitModifier = 'is-primary';
+  export let payload = {};
+  export let disabled = false;
+  export let bulmaSubmitModifier = "is-primary";
 
-    const dispatch = createEventDispatcher();
-    const handleSubmit = () => { dispatch('submit', payload); };
-    const handleCancel = () => { dispatch('cancel'); };
+  const dispatch = createEventDispatcher();
+  const handleSubmit = () => {
+    dispatch("submit", payload);
+  };
+  const handleCancel = () => {
+    dispatch("cancel");
+  };
 </script>
 
 <div class="field is-grouped">
 
-    <div class="control">
-        <button type="submit" class="button {bulmaSubmitModifier}" on:click={handleSubmit} disabled={disabled}>Submit</button>
-    </div>
+  <div class="control">
+    <button
+      style="background-color: #50A3D5"
+      type="submit"
+      class="button {bulmaSubmitModifier}"
+      on:click={handleSubmit}
+      {disabled}>
+      Submit
+    </button>
+  </div>
 
-    <div class="control">
-        <button class="button is-light" on:click={handleCancel}>Cancel</button>
-    </div>
+  <div class="control">
+    <button class="button is-light" on:click={handleCancel}>Cancel</button>
+  </div>
 
 </div>
--- src/routes/Login.svelte
+++ src/routes/Login.svelte
@@ -1,65 +1,111 @@
 <script>
-    import { replace } from 'svelte-spa-router';
-    import { key, userId, user } from '../stores/auth.js';
-    import { makeAxios, makeAxiosWithKey } from '../axios.js';
-    import Submit from '../components/Submit.svelte';
+  import { replace } from "svelte-spa-router";
+  import { key, userId, user } from "../stores/auth.js";
+  import { makeAxios, makeAxiosWithKey } from "../axios.js";
+  import Submit from "../components/Submit.svelte";
 
-    let email = "customer0@email.com";
-    let password = "password";
+  let email = "customer0@email.com";
+  let password = "password";
 
-    let axios = makeAxios();
+  let axios = makeAxios();
 
-    const handleSubmit = async () => {
+  const handleSubmit = async () => {
+    try {
+      let response = await axios.post("/login", { email, password });
+      key.set(response.data.key);
+      userId.set(response.data.userId);
 
-        try {
+      let authAxios = makeAxiosWithKey(response.data.key);
+      response = await authAxios.get(`/users/${response.data.userId}`);
+      user.set(response.data);
 
-            let response = await axios.post('/login', {email, password})
-            key.set(response.data.key);
-            userId.set(response.data.userId);
+      replace("/dashboard");
+    } catch (err) {
+      console.log(err);
+      alert("Login failed");
+    }
+  };
 
-            let authAxios = makeAxiosWithKey(response.data.key);
-            response = await authAxios.get(`/users/${response.data.userId}`)
-            user.set(response.data);
-
-            replace('/dashboard');
-
-        } catch (err) {
-            console.log(err);
-            alert("Login failed");
-        }
-    };
-
-    const handleCancel = () => {
-        email = "";
-        password = "";
-        key.reset();
-    };
+  const handleCancel = () => {
+    email = "";
+    password = "";
+    key.reset();
+  };
 </script>
 
-<section class="section">
-    <div class="container">
-        <h1 class="title">Login</h1>
-
-        <form class="form">
-            <div class="field">
-                <div class="control">
-                    <input class="input" type="email" placeholder="user@email.com" bind:value={email} />
-                </div>
-            </div>
-
-            <div class="field">
-                <div class="control">
-                    <input class="input" type="password" placeholder="Passowrd" bind:value={password} />
-                </div>
-            </div>
-
-            <Submit on:submit={handleSubmit} on:cancel={handleCancel} bulmaSubmitModifier='is-link' />
-        </form>
-    </div>
-</section>
-
 <style>
-    .container {
-        max-width: 480px;
-    }
+  .section {
+    height: 100%;
+    width: 100%;
+    padding-top: 100px;
+    background-image: url("/img/login_background.jpg");
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-size: cover;
+  }
+  #login_box {
+    width: 480px;
+    padding: 10px;
+    border: 5px solid darkslategrey;
+    border-radius: 5px;
+    background-color: whitesmoke;
+  }
+  c2 {
+    color: #fcba03;
+  }
+  img {
+    max-width: 100px;
+    max-height: 100px;
+  }
 </style>
+
+<section class="section">
+
+  <div class="container" id="login_box">
+
+    <div class="media">
+      <img src="/img/logo.png" alt="logo" class="media-left" />
+      <div class="media-content">
+        <h1 class="title is-2">
+          <em>
+            Welcome to
+            <c2>Adventureland!</c2>
+          </em>
+        </h1>
+      </div>
+
+    </div>
+
+    <hr style="border: solid darkslategrey 2px;" />
+
+    <h2 class="title is-3">Login</h2>
+
+    <form class="form">
+      <div class="field">
+        <div class="control">
+          <input
+            class="input"
+            type="email"
+            placeholder="user@email.com"
+            bind:value={email} />
+        </div>
+      </div>
+
+      <div class="field">
+        <div class="control">
+          <input
+            class="input"
+            type="password"
+            placeholder="Passowrd"
+            bind:value={password} />
+        </div>
+      </div>
+
+      <Submit
+        on:submit={handleSubmit}
+        on:cancel={handleCancel}
+        bulmaSubmitModifier="is-link" />
+    </form>
+  </div>
+
+</section>
